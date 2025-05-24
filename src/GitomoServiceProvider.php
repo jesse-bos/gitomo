5e9264898785cd3e186e89384e33d2866623b6dc
jesse-bos <131663982+jesse-bos@users.noreply.github.com>
2025-05-24 12:59:11 +0200

    Remove unused code

--- src/GitomoServiceProvider.php
+++ src/GitomoServiceProvider.php
@@ -10,16 +10,9 @@ class GitomoServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('gitomo')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_gitomo_table')
             ->hasCommand(GitomoCommand::class);
     }
 }
